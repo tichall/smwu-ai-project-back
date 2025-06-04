c7d3e2f161c0e1f812ccb3cda3d7e103791d0fe5
tichall <0817frog@gmail.com>
2025-06-04 15:22:00 +0900

    [feature] #3 : 추천받은 금융상품 데이터 구조 설계

---
+++ src/main/java/smwu/server/domain/entity/UserRecommendation.java
@@ -0,0 +1,30 @@
+package smwu.server.domain.entity;
+
+import com.azure.spring.data.cosmos.core.mapping.Container;
+import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Container(containerName = "user_recommendations")
+public class UserRecommendation {
+    @Id
+    private String id;
+
+    @PartitionKey
+    private String userId;
+
+    private String message;
+    private OffsetDateTime recommendedAt;
+
+    private List<RecommendedItem> recommendedProducts;
+
+    @Getter @NoArgsConstructor @AllArgsConstructor @Builder
+    public static class RecommendedItem {
+        private String productId;
+        private boolean liked;
+    }
+}
+
